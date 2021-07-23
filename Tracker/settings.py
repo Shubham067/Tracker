a8dff746519ecd44cfa91f2b5d63e81c7e8afae1
Shubham067 <shubhamchandra067@gmail.com>
2021-07-23 13:30:12 +0530

    minor fix

--- Tracker/settings.py
+++ Tracker/settings.py
@@ -54,6 +54,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+SECURE_SSL_REDIRECT = True
+
 SECURE_BROWSER_XSS_FILTER = True
 
 X_FRAME_OPTIONS = 'DENY'
